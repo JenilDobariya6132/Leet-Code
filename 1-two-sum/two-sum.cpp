c827c8dd51012d750e1d920c298022cacddd8373
Jenil Dobariya <jenil.dobariya126194@marwadiuniversity.ac.in>
2026-08-08 10:57:37 +0530

    Time: 1 ms (76.76%) | Memory: 14.8 MB (40.81%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> mp;
 
         for (int i = 0; i < nums.size(); i++) {
-            int complement = target -nums[i];
+            int complement = target - nums[i];
 
             if (mp.find(complement) != mp.end()) {
                 return {mp[complement], i};
