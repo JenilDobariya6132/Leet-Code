fc2cf78e30b255a48f5380c1c4522cc6e5e6ee1b
Jenil Dobariya <jenil.dobariya126194@marwadiuniversity.ac.in>
2026-08-14 16:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (68.39%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> frequency;
+
+        for (int num : nums) {
+            frequency[num]++;
+        }
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > minHeap;
+
+        for (auto& [num, count] : frequency) {
+
+            minHeap.push({count, num});
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        vector<int> result;
+
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
+        return result;
+    }
+};
